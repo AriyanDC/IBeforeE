23c171940d4ac3b9dd1bb5fa1d8d7f3a9058619f
AriyanDC <ariyan.vafaie@gmail.com>
2022-09-17 15:16:59 +0300

    Numbers of CEI, EI, IE are printed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/words_alpha.txt"));
+
+        ArrayList<String> wordsCEI = new ArrayList<String>();
+        ArrayList<String> wordsEI = new ArrayList<String>();
+        ArrayList<String> wordsIE = new ArrayList<String>();
+
+        String l = bufferedReader.readLine();
+        int lineIndex = 1;
+        while (l != null) {
+//            if (lineIndex == 100000) break;
+            if (l.contains("cei")) wordsCEI.add(l);
+            if (l.contains("ei")) wordsEI.add(l);
+            if (l.contains("ie")) wordsIE.add(l);
+
+            lineIndex++;
+            l = bufferedReader.readLine();
+        }
+
+        System.out.println("CEI: " + wordsCEI.size());
+        System.out.println("EI: " + wordsEI.size());
+        System.out.println("IE: " + wordsIE.size());
+        // ei: 4351 ie:13310 cei: 251
     }
 }
